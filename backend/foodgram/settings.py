85d2ed918d63f9ba9452ae14c3a6022688449c76
Anna Tsoy <anna4430319@gmail.com>
2024-09-25 20:46:37 +0300

    First Deploy

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -115,7 +115,7 @@ STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# Аналог MEDIA_ROOT = BASE_DIR / 'media'
+# аналог MEDIA_ROOT = BASE_DIR / 'media'
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
