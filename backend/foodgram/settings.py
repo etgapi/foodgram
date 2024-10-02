e57a3b4b4dd3683934d53534b5b7fe6a9dc8bad6
Anna Tsoy <anna4430319@gmail.com>
2024-10-03 00:46:31 +0300

    Deploy Attempt-8

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -114,8 +114,14 @@ USE_L10N = True
 USE_TZ = True
 
 
+# При планировании архитектуры было решено,
+# что статические файлы Django должны быть доступны по адресу /static/
 STATIC_URL = "/static/"
+# Указываем корневую директорию для сборки статических файлов;
+# в контейнере это будет /app/collected_static
 STATIC_ROOT = BASE_DIR / "collected_static"
+# Теперь при вызове команды python manage.py collectstatic
+# Django будет копировать все статические файлы в директорию collected_static
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
