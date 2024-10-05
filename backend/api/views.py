e23eb4f79a9092d896e0b13e7fa8f14fc48962f7
Anna Tsoy <anna4430319@gmail.com>
2024-10-05 11:36:25 +0300

    Deploy Attempt-16

--- backend/api/views.py
+++ backend/api/views.py
@@ -223,7 +223,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 .all()
             )
 
-        return recipes.order_by("-creation_date").all()
+        return recipes.order_by("-creation_date")
 
     @action(
         methods=["post"],
